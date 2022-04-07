6cc1e3c628447c9f95d82eb10795eb0fe1d9b2c8
morkovkalove <73894426+morkovkalove@users.noreply.github.com>
2022-04-07 14:53:56 +0300

    Update User.cs

--- User.cs
+++ User.cs
@@ -2,6 +2,16 @@
 {
     public class User
     {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
         
     }
 }
